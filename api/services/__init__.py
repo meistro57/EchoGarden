97a855303b1620f239def7602bd54fd66562f774
Meistro <meistro57@gmail.com>
2025-10-23 22:40:02 -0500

    Add conversation discovery and search helpers

--- api/services/__init__.py
+++ api/services/__init__.py
@@ -2,5 +2,11 @@
 """Service layer utilities for the EchoGarden API."""
 
 from .normalization import normalize_message, redact_text
+from .search import build_highlights, parse_search_terms
 
-__all__ = ["normalize_message", "redact_text"]
+__all__ = [
+    "normalize_message",
+    "redact_text",
+    "build_highlights",
+    "parse_search_terms",
+]
