294fc8a0e11153df6cbbb97bb5c006b583e3ac76
Meistro <meistro57@gmail.com>
2025-10-24 08:53:28 -0500

    Fix UI dependency installation by removing Radix packages

---
+++ ui/next-env.d.ts
@@ -0,0 +1,6 @@
+// ui/next-env.d.ts
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+/// <reference types="next/navigation-types/compat/navigation" />
+
+// NOTE: This file should not be edited as it ensures Next.js types are available in the project.
